d5b742542d28969bd746b41e0e4cf15869fe0471
Rami Del Toro <rami.deltoro@gmail.com>
2026-06-19 00:40:43 -0400

    Track Xcode project files directly

---
+++ NutsNews/NutsNews/NutsNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutsNewsApp.swift
+//  NutsNews
+//
+//  Created by Rami Del Toro on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NutsNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
